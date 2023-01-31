83c50089152266f11e50f2e65696a8b2b5dd85fe
Luis Lopez <luis.lopez@altoros.com>
2023-01-31 17:16:09 -0300

    Add more decimal helpers

--- tests/dummy/tests/decimals.test.ts
+++ tests/dummy/tests/decimals.test.ts
@@ -1,4 +1,4 @@
-import { describe, test } from "matchstick-as/assembly/index";
+import { describe, test, assert } from "matchstick-as/assembly/index";
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import { decimals, tests } from "../../../assembly";
 
@@ -20,6 +20,36 @@ describe("Decimals", () => {
       tests.asserts.decimalEquals(
         decimals.fromBigInt(value, 6),
         BigDecimal.fromString("1")
+      );
+    });
+  });
+  describe("fromFloat", () => {
+    test("returns BigDecimal from Float", () => {
+      const value = F64.MAX_VALUE;
+
+      tests.asserts.decimalEquals(
+        BigDecimal.fromString("1.7976931348623157e+308"),
+        decimals.fromFloat(value)
+      );
+    });
+  });
+  describe("fromInt", () => {
+    test("returns BigDecimal from Integer", () => {
+      const value = I64.MAX_VALUE;
+
+      tests.asserts.decimalEquals(
+        BigDecimal.fromString("9223372036854775807"),
+        decimals.fromInt(value)
+      );
+    });
+  });
+  describe("toBigInt", () => {
+    test("converts BigDecimal into BigInt", () => {
+      const value = BigDecimal.fromString("100.525678");
+
+      assert.bigIntEquals(
+        BigInt.fromString("100525678"),
+        decimals.toBigInt(value)
       );
     });
   });
