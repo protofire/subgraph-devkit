a9df21f57cf891535acebbcb34ab58f0e68490ad
Luis Lopez <luigibyte@gmail.com>
2023-01-31 17:29:21 -0300

    Decimals helpers (#15)
    * Add fromFloat, fromInt, toBigInt helpers
    * bump up version to 1.1.0

--- assembly/decimals.ts
+++ assembly/decimals.ts
@@ -2,9 +2,21 @@ import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import { TEN } from "./integers";
 
 export function fromBigInt(value: BigInt, decimals: u8): BigDecimal {
-  const precision = TEN.pow(decimals).toBigDecimal();
+  const precision = TEN.pow(decimals);
 
-  return value.divDecimal(precision);
+  return value.divDecimal(precision.toBigDecimal());
+}
+
+export function fromFloat(value: f64): BigDecimal {
+  return BigDecimal.fromString(value.toString());
+}
+
+export function fromInt(value: i64): BigDecimal {
+  return BigDecimal.fromString(value.toString());
+}
+
+export function toBigInt(value: BigDecimal): BigInt {
+  return value.digits;
 }
 
 export const ZERO = BigDecimal.fromString("0");
